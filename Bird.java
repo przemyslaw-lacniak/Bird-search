ae050fdded5de9a54d21375a3bdff8d86d57ffe2
przemyslaw-lacniak <79154557+przemyslaw-lacniak@users.noreply.github.com>
2021-03-03 16:25:22 +0100

    Create Bird.java

---
+++ Bird.java
@@ -0,0 +1,37 @@
+
+public class Bird {
+    
+    private String name;
+    private String nameInLatin;
+    private int observations;
+    
+    public Bird(String name, String nameInLatin) {
+        this.name = name;
+        this.nameInLatin = nameInLatin;
+        this.observations = 0;
+    }
+    
+    public void Observation() {
+        this.observations++;
+    }
+
+    public int getObservations() {
+        return observations;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " (" + this.nameInLatin + "): " + this.observations + " observations";
+    }
+    
+     
+    
+
+
+
+
+}
